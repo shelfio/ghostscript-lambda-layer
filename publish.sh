8f47964c5c6b8b78390eaf471ceb7b67e89fea44
Vlad Holubiev <golubev.vld@gmail.com>
2020-02-13 17:59:24 +0200

    fix: unify publish script

--- publish.sh
+++ publish.sh
@@ -2,6 +2,14 @@
 
 GHOSTSCRIPT_VERSION=9.50
 LAYER_NAME='ghostscript'
+LAYER_VERSION=$(
+  aws lambda publish-layer-version --region "$TARGET_REGION" \
+    --layer-name $LAYER_NAME \
+    --zip-file fileb://ghostscript.zip \
+    --description "Ghostscript v${GHOSTSCRIPT_VERSION}" \
+    --query Version \
+    --output text
+)
 
 aws lambda add-layer-version-permission \
   --region "$TARGET_REGION" \
@@ -11,12 +19,4 @@ aws lambda add-layer-version-permission \
   --principal '*' \
   --query Statement \
   --output text \
-  --version-number "$(
-    aws lambda publish-layer-version \
-      --region="$TARGET_REGION" \
-      --layer-name="$LAYER_NAME" \
-      --zip-file="fileb://ghostscript.zip" \
-      --description="Ghostscript v${GHOSTSCRIPT_VERSION}" \
-      --query="Version" \
-      --output="text"
-  )"
+  --version-number "$LAYER_VERSION"
