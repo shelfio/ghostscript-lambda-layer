44c98408e730f41a0fcf7672f1569e66ce7f92ea
Yemi-mp <opeyemi.adenola@moonpig.com>
2025-05-19 08:27:44 +0100

    extended for clarity

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+set -e
 
+echo "Building Docker image..."
 docker build -t gs-lambda-layer .
-docker run --rm gs-lambda-layer cat /tmp/gs.zip > ./ghostscript.zip
+
+echo "Creating container..."
+container_id=$(docker create gs-lambda-layer sleep 1)
+
+echo "Copying built Lambda layer ZIP to host..."
+docker cp "$container_id":/tmp/ghostscript.zip ./ghostscript.zip
+
+echo "Cleaning up container..."
+docker rm "$container_id"
